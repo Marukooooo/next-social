9df21a97747ba4a77bd18eee3cc916f1601fb527
Marukooooo <133352243+Marukooooo@users.noreply.github.com>
2024-07-29 01:17:19 +0800

    first commit

---
+++ src/components/Post.tsx
@@ -0,0 +1,89 @@
+import Image from "next/image";
+import Comments from "./Comments";
+
+const Post = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      {/* user */}
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-4">
+          <Image
+            src="https://images.pexels.com/photos/26796134/pexels-photo-26796134.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={40}
+            height={40}
+            className="w-10 h-10 rounded-full"
+          />
+          <span className="font-medium">Jack McBride</span>
+        </div>
+        <Image src="/more.png" alt="" width={16} height={16} />
+      </div>
+      {/* DESC */}
+      <div className="flex flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            src="https://images.pexels.com/photos/27086057/pexels-photo-27086057.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load
+            "
+            alt=""
+            fill
+            className="object-cover rounded-md"
+          />
+        </div>
+        <p>
+          VS Code supports almost every major programming language. Several ship
+          in the box, like JavaScript, TypeScript, CSS, and HTML, but extensions
+          for others can be found in the VS Code Marketplace.
+        </p>
+      </div>
+      {/* INTERACTION */}
+      <div className="flex items-center justify-between text-sm my-4">
+        <div className="flex gap-8">
+          <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/like.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Likes</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/comment.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Comments</span>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <div className="flex items-center gap-4 bg-slate-100 p-2 rounded-xl">
+            <Image
+              src="/share.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">
+              123<span className="hidden md:inline"> Shares</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Comments />
+    </div>
+  );
+};
+
+export default Post;
